86ac691fd0b5cabfd31054d73da2abc39b9f2cc9
Mekire <3ror@live.com>
2014-12-03 11:55:50 +0900

    Small functions added; needs additional testing.

--- rect.js
+++ rect.js
@@ -79,7 +79,83 @@ RECT.Rect = function(x, y, w, h){
     RECT._makeMultiProperty(this, 2, 3, 'size', 'w', 'h');
 };
 
-RECT.Rect.prototype.collidePoint = function(x,y){
+RECT.Rect.prototype.copy = function(){
+    /*
+     * Returns a copy of the rect.
+     */
+    return new RECT.Rect(this.x, this.y, this.w, this.h);
+};
+
+RECT.Rect.prototype.containsRect = function(other){
+    /*
+     * Returns true if a rect 'other' fits entirely inside 'this'.    
+     */  
+    return (other.x >= this.x && other.right <= this.right &&
+            other.y >= this.y && other.bottom <= this.bottom);
+};
+
+RECT.Rect.prototype.move = function(x, y){
+    /*
+     * Returns a new rectangle that is moved by the given offset.
+     */
+    return new RECT.Rect(this.x+x, this.y+y, this.w, this.h);  
+};
+
+RECT.Rect.prototype.moveIP = function(x, y){
+    /*
+     * Moves the rectangle inplace by the given offset.
+     */
+    this.x += x;
+    this.y += y;
+};
+
+RECT.Rect.prototype.inflate = function(x, y){
+    /*
+     * Returns a new rect enlarged or shrank by the given arguments.
+     * The center of the rect will remain the same.
+     */
+    var inflated = new RECT.Rect(this.x, this.y, this.w+x, this.h+y);
+    inflated.center = this.center;
+    return inflated;
+};
+
+RECT.Rect.prototype.inflateIP = function(x, y){
+    /*
+     * Enlarge or shrink the rectangle inplace.
+     * The center of the rect will remain the same.
+     */
+    var originalCenter = this.center;
+    this.w += x;
+    this.h += y;
+    this.center = originalCenter;
+};
+
+RECT.Rect.prototype.clamp = function(other){
+    /*
+     * Returns a new rectangle that is moved to be completely inside the 
+     * argument Rect. If the rectangle is too large to fit inside, it is 
+     * centered inside the argument Rect, but its size is not changed.
+     */
+    var clamped = this.copy();
+    clamped.clampIP(other);
+    return clamped;
+};
+
+RECT.Rect.prototype.clampIP = function(other){
+    /*
+     * Rect is moved inplace to be completely inside the argument Rect. 
+     * If the rectangle is too large to fit inside, it is centered inside 
+     * the argument Rect, but its size is not changed.
+     */
+    this.x = Math.max(this.x, other.x);
+    this.y = Math.max(this.y, other.y);
+    this.right = Math.min(this.right, other.right);
+    this.bottom = Math.min(this.bottom, other.bottom);
+    if(!other.containsRect(this))
+        this.center = other.center;
+};
+
+RECT.Rect.prototype.collidePoint = function(x, y){
     /* 
      * Check if a point (given by x and y) overlaps the rectangle.
      * The left and top edge are inclusive; the bottom and right edge are not.
@@ -104,8 +180,6 @@ RECT._makePropertyAlias = function(obj, original, alias, enumerate){
      * Create a property obj.alias that gets and sets the property 
      * original.alias (eg set and get Rect.w using Rect.width).
      */
-    if(enumerate === undefined)
-        enumerate = false;
     Object.defineProperty(obj, alias, {
         enumerable : enumerate,
         get: function(){return obj[original];},
@@ -118,7 +192,8 @@ RECT._makeMultiProperty = function(obj, prop1, prop2, name, var1, var2){
     /*
      * Create a property name that gets and sets both obj.prop1 and obj.prop2.
      * var1 and var2 can be used to set the attributes needed when assigning
-     * by an explicit object rather than a two element array. 
+     * by an explicit object rather than a two element array. var1 and var2
+     * default to 'x' and 'y' if not provided.
      * 
      * eg 
      *     RECT._makeMultiProperty(this, 'w', 'h', 'size', 'w', 'h');
@@ -128,10 +203,8 @@ RECT._makeMultiProperty = function(obj, prop1, prop2, name, var1, var2){
      * or
      *     obj.size = {w: 10, h: 30}; // object with attributes var1 and var2
      */
-    if(var1 === undefined)
-        var1 = "x";
-    if(var2 === undefined)
-        var2 = "y";
+    var1 = var1 || 'x';
+    var2 = var2 || 'y';
     Object.defineProperty(obj, name, {
         get: function(){return [obj[prop1], obj[prop2]];},
         set: function(value){
